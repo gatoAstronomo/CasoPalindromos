7c0c83171ed111f6f8fbd64828d9756afaa209dc
Arturo Rivas <a.rivas06@ufromail.cl>
2024-04-04 17:05:45 -0300

    esPalindromo version corregida, pasa los testcases

--- src/main/java/proyect/CasoPalindromos.java
+++ src/main/java/proyect/CasoPalindromos.java
@@ -2,6 +2,8 @@ package proyect;
 
 public class CasoPalindromos{
     public static boolean esPalindromo(String cadena){
+        cadena = cadena.toLowerCase().replaceAll("\\s","");
+
         StringBuilder invertida = new StringBuilder(cadena).reverse();
         return  cadena.contentEquals(invertida);
     }
